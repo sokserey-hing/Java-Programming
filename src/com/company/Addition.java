235fb0d0578ad6d8825b7f91ecdfb15629b86005
Hing Sokserey <soksereihing@gmail.com>
2021-10-21 13:45:11 +0530

    addition program

---
+++ src/com/company/Addition.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Addition {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = a + b;
+        System.out.println("addition is "+c);
+    }
+}
