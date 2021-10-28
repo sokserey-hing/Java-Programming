936a430a0c5ce595b10c4d4b0027dee308a1921a
Hing Sokserey <soksereihing@gmail.com>
2021-10-28 10:32:48 +0530

    Addes ReadMe.txt
    Some Main

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("IntelliJ and Git is cool");
+        System.out.println("IntelliJ and Git is cool");
     }
 }
